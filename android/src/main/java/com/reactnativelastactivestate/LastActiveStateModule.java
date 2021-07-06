5976cd67792e7be30471a10c180c32a523cf8e2b
александр чернов <aleksandr.nikolaevich.ch@gmail.com>
2021-07-06 17:30:34 +0300

    chore: initial commit

---
+++ android/src/main/java/com/reactnativelastactivestate/LastActiveStateModule.java
@@ -0,0 +1,34 @@
+package com.reactnativelastactivestate;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = LastActiveStateModule.NAME)
+public class LastActiveStateModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "LastActiveState";
+
+    public LastActiveStateModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  LastActiveStateExample
+//
+
+import Foundation
